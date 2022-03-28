4890a97f4d7ddae68a78f7d6b92637ab10765035
Aleksandr Tikhonov <t9101029991@gmail.com>
2022-03-28 17:32:22 +0300

    add 4 blocks

--- src/data.js
+++ src/data.js
@@ -1,8 +1,29 @@
 module.exports = {
+  domain: "mebeldomann.ru",
+  contacts: {
+    phone: "8-800-245-12-43",
+    city: "Москва",
+    adress: "ул. Гурьянова, 51",
+  },
+  schedule: "c 8:00 до 20:00",
   products: [
     { name: "product 1", price: 123 },
     { name: "product 2", price: 123423 },
     { name: "product 3", price: 123 },
     { name: "product 4", price: 123 },
+  ],
+  headerLinks: [
+    { name: "Каталог мебели", url: "#" },
+    { name: "Доставка", url: "#delivery" },
+    { name: "Оплата и заказ", url: "#" },
+    { name: "Гарантии и возврат", url: "#" },
+    { name: "Контакты", url: "#contact" },
+  ],
+  footerLinks: [
+    { name: "Каталог мебели", url: "#" },
+    { name: "Доставка", url: "#delivery" },
+    { name: "Оплата и заказ", url: "#" },
+    { name: "Гарантии и возврат", url: "#" },
+    { name: "Контакты", url: "#contact" },
   ],
 };
