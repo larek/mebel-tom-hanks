0d08b8604ed653d8b56ef654641254718ca2e2ae
Aleksandr Tikhonov <t9101029991@gmail.com>
2022-03-29 13:19:09 +0300

    update some blocks

--- src/data.js
+++ src/data.js
@@ -7,21 +7,158 @@ module.exports = {
     adress: "ул. Гурьянова, 51",
   },
   schedule: "c 8:00 до 20:00",
-  products: [
-    { name: "product 1", price: 123 },
-    { name: "product 2", price: 123423 },
-    { name: "product 3", price: 123 },
-    { name: "product 4", price: 123 },
+  categories: [
+    {
+      name: "Шкафы",
+      products: [
+        { name: "Шкаф-купе Бася с зеркалом", image: "holder.jpg" },
+        {
+          name: "Распашной шкаф для одежды и белья",
+          image: "holder.jpg",
+        },
+        { name: "Узкий стеллаж (6 секций)", image: "holder.jpg" },
+        {
+          name: "Шкаф витрина со шторами для гостиной",
+          image: "holder.jpg",
+        },
+      ],
+      categories: [
+        { name: "Шкафы-купе", link: "#" },
+        { name: "Стеллажи", link: "#" },
+        { name: "Шкафы-пеналы", link: "#" },
+        { name: "Книжные шкафы", link: "#" },
+      ],
+    },
+    {
+      name: "Мягкая мебель",
+      products: [
+        {
+          name: "Угловой диван аккордеон Мэдисон с подушками",
+          image: "holder.jpg",
+        },
+        { name: "Кожаный офисный диван книжка", image: "holder.jpg" },
+        {
+          name: "Современный круглый пуф на металлических ножках",
+          image: "holder.jpg",
+        },
+        {
+          name: "Маленькая серая тахта (обивка - текстиль)",
+          image: "holder.jpg",
+        },
+      ],
+      categories: [
+        { name: "Мягкие кресла", link: "#" },
+        { name: "Прямые диваны", link: "#" },
+        { name: "Пуфы и банкетки", link: "#" },
+      ],
+    },
+    {
+      name: "Мебель для спальни",
+      products: [
+        { name: "Прикроватная тумба с ящиками", image: "holder.jpg" },
+        {
+          name: "Двуспальная кровать с подъемным механизмом",
+          image: "holder.jpg",
+        },
+        {
+          name: "Ортопедический матрас на кровать (спальное место 160х200 см)",
+          image: "holder.jpg",
+        },
+        {
+          name: "Белый туалетный столик из ЛДСП с полками для хранения",
+          image: "holder.jpg",
+        },
+      ],
+      categories: [
+        { name: "Туалетные столики", link: "#" },
+        { name: "Комоды", link: "#" },
+        { name: "Кровати", link: "#" },
+        { name: "Матрасы", link: "#" },
+      ],
+    },
+    {
+      name: "Мебель для кухни",
+      products: [
+        { name: "Барный стул с подставкой", image: "holder.jpg" },
+        {
+          name: "Обеденный раскладной стол для кухни из дерева",
+          image: "holder.jpg",
+        },
+        { name: "Деревянный кухонный уголок", image: "holder.jpg" },
+        { name: "Кухня Уют модерн (дуб сонома)", image: "holder.jpg" },
+      ],
+      categories: [
+        { name: "Кухонные уголки", link: "#" },
+        { name: "Кухонные гарнитуры", link: "#" },
+        { name: "Обеденные столы", link: "#" },
+        { name: "Кухонные стулья и табуреты", link: "#" },
+      ],
+    },
+    {
+      name: "Мебель для гостинной",
+      products: [
+        {
+          name: "Дизайнерский журнальный стол трансформер Терра (венге)",
+          image: "holder.jpg",
+        },
+        {
+          name: "Напольная тумба под телевизор для гостиной",
+          image: "holder.jpg",
+        },
+        {
+          name: "Cтенка-горка для гостиной Марта в стиле Прованс",
+          image: "holder.jpg",
+        },
+        { name: "Подвесная тумба в гостиную", image: "holder.jpg" },
+      ],
+      categories: [
+        { name: "Журнальные столики", link: "#" },
+        { name: "Тумбы под ТВ", link: "#" },
+        { name: "Стенки", link: "#" },
+        { name: "Настенные полки", link: "#" },
+      ],
+    },
+    {
+      name: "Мебель для детской",
+      products: [
+        { name: "Шкаф для детской комнаты", image: "holder.jpg" },
+        {
+          name: "Детское компьютерное кресло (материал - ткань)",
+          image: "holder.jpg",
+        },
+        { name: "Пуф (кресло-мешок) для детской комнаты", image: "holder.jpg" },
+        { name: "Столик для творчества со стулом", image: "holder.jpg" },
+      ],
+      categories: [
+        { name: "Кровати для новорожденных", link: "#" },
+        { name: "Детские односпальные кровати", link: "#" },
+        { name: "Детские комоды", link: "#" },
+        { name: "Растущие стулья для детей", link: "#" },
+      ],
+    },
+    {
+      name: "Мебель для прихожей",
+      products: [
+        { name: "Большой шкаф-купе для дома", image: "holder.jpg" },
+        { name: "Модульная прихожая со шкафом", image: "holder.jpg" },
+        { name: "Шкаф стеллаж из металла Лофт", image: "holder.jpg" },
+      ],
+      categories: [
+        { name: "Вешалки", link: "#" },
+        { name: "Тумбы для обуви (обувницы)", link: "#" },
+        { name: "Готовые прихожие", link: "#" },
+      ],
+    },
   ],
   headerLinks: [
-    { name: "Каталог мебели", url: "#" },
+    { name: "Каталог мебели", url: "#categories" },
     { name: "Доставка", url: "#delivery" },
     { name: "Оплата и заказ", url: "#" },
     { name: "Гарантии и возврат", url: "#" },
     { name: "Контакты", url: "#contact" },
   ],
   footerLinks: [
-    { name: "Каталог мебели", url: "#" },
+    { name: "Каталог мебели", url: "#categories" },
     { name: "Доставка", url: "#delivery" },
     { name: "Оплата и заказ", url: "#" },
     { name: "Гарантии и возврат", url: "#" },
