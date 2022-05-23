e3832f424e7eba831c45c13bbfe2fece5b7b8db9
Aleksandr Tikhonov <t9101029991@gmail.com>
2022-05-24 01:10:38 +0300

    add 2 dataset, eslint and robots generator

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
-const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
-const path = require("path");
-const { writeFileSync, existsSync } = require("fs");
-const { build } = require("vite");
-const SRC = path.join(__dirname, "../src");
-const arg = require("arg");
-const args = arg({ "--source": String });
-const dataSource = args["--source"] ?? "data-landing-1.js";
+const { TwingEnvironment, TwingLoaderFilesystem } = require("twing")
+const path = require("path")
+const { writeFileSync, readFileSync } = require("fs")
+const { build } = require("vite")
+const SRC = path.join(__dirname, "../src")
+const PUBLIC_DIR = path.join(__dirname, "../public")
+const arg = require("arg")
+const args = arg({ "--source": String, "--robots": Boolean })
+const dataSource = args["--source"] ?? "data-landing-1.js"
+const robotsSource = args["--robots"] ? SRC + "/robots-allow.txt" : SRC + "/robots-disallow.txt"
+const robots = readFileSync(robotsSource);
 
 (async () => {
-  const loader = new TwingLoaderFilesystem(SRC);
-  const twing = new TwingEnvironment(loader);
-  const data = await require(path.join(__dirname, `../src/${dataSource}`));
-  const html = await twing.render("index.twig", { data });
-  writeFileSync(SRC + "/index.html", html);
+  const loader = new TwingLoaderFilesystem(SRC)
+  const twing = new TwingEnvironment(loader)
+  const data = await require(path.join(__dirname, `../src/${dataSource}`))
+  const html = await twing.render("index.twig", { data })
+  writeFileSync(PUBLIC_DIR + "/robots.txt", robots)
+  writeFileSync(SRC + "/index.html", html)
   await build({
     root: SRC,
     publicDir: path.join(__dirname, "../public"),
     build: {
       outDir: path.join(__dirname, "../dist"),
-      emptyOutDir: true,
-    },
-  });
-})();
+      emptyOutDir: true
+    }
+  })
+})()
--- scripts/server.js
+++ scripts/server.js
@@ -1,50 +1,50 @@
 #!/usr/bin/env node
 
-const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
-const path = require("path");
-const { writeFileSync, existsSync } = require("fs");
-const SRC = path.join(__dirname, "../src");
-const { watch } = require("chokidar");
-const { createServer } = require("vite");
-const arg = require("arg");
-const args = arg({ "--source": String });
-const dataSource = args["--source"] ?? "data-landing-1.js";
+const { TwingEnvironment, TwingLoaderFilesystem } = require("twing")
+const path = require("path")
+const { writeFileSync } = require("fs")
+const SRC = path.join(__dirname, "../src")
+const { watch } = require("chokidar")
+const { createServer } = require("vite")
+const arg = require("arg")
+const args = arg({ "--source": String })
+const dataSource = args["--source"] ?? "data-landing-1.js"
 
 const createHtml = async () => {
-  const loader = new TwingLoaderFilesystem(SRC);
-  const twing = new TwingEnvironment(loader);
-  const data = await require(path.join(__dirname, `../src/${dataSource}`));
-  const html = await twing.render("index.twig", { data });
-  writeFileSync(SRC + "/index.html", html);
-};
+  const loader = new TwingLoaderFilesystem(SRC)
+  const twing = new TwingEnvironment(loader)
+  const data = await require(path.join(__dirname, `../src/${dataSource}`))
+  const html = await twing.render("index.twig", { data })
+  writeFileSync(SRC + "/index.html", html)
+}
 
 // Create dev server
 const createDevServer = async (watcher) => {
-  createHtml();
+  createHtml()
 
-  let server = await createServer({
+  const server = await createServer({
     mode: "development",
     watcher,
     root: SRC,
     publicDir: path.join(__dirname, "../public"),
     server: {
       port: 1337,
-      open: true,
-    },
-  });
+      open: true
+    }
+  })
 
-  await server.listen();
+  await server.listen()
 
-  server.printUrls();
-};
+  server.printUrls()
+}
 
 // Create watcher
 const watcher = watch(SRC, {
   ignored: /index.html/,
   persistent: true,
-  ignoreInitial: true,
-});
+  ignoreInitial: true
+})
 
 // Add watcher listeners
-watcher.on("ready", () => createDevServer(watcher));
-watcher.on("change", () => createHtml());
+watcher.on("ready", () => createDevServer(watcher))
+watcher.on("change", () => createHtml())
